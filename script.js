7578054933fbb2703657c343a46f34adebd9a671
NatedoG-byte <Nathanieldavis@bmc.edu>
2025-10-21 21:49:10 -0500

    initial

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 console.log("Hello World!");
 // 1. Create an array called favoriteFoods with at least 6 foods you love.
-
+let favoriteFoods = [mango, Sushi, steak, pizza, peanutbutter, cookies]
 
 
 // 2. Loop through the list and print: "One of my favorite foods is ______."
-
+for (let food of favoriteFoods) {
+  console.log("One of my favorite foods is " + food + ".");
+}
 
 
 
@@ -36,24 +38,40 @@ let friendFavorites = [
   ];
 
 // 5. Print out only foods that have an "a" in the name. For example, "Pizza" would not be included, but "Pasta" would be.
-
+for (let food of friendFavorites) {
+  if (food.toLowerCase().includes("a")) {
+    console.log(food);
+  }
+}
 
 
 // 6. Store the result in an array called foodsWithA. Print out the array.
-
+let foodsWithA = friendFavorites.filter(food => food.toLowerCase().includes("a"));
+console.log("Foods with 'a':", foodsWithA);
 
 
 // 7. Create a new array longFoodNames for foods with names longer than 6 characters.
-
+let longFoodNames = friendFavorites.filter(food => food.length > 6);
 
 
 // 8. Create another array shortFoodNames for foods 6 characters or shorter.
-
+let shortFoodNames = friendFavorites.filter(food => food.length <= 6);
 
 
 // 9. Print both arrays and compare:
-// "There are more long-named foods." OR "There are more short-named foods."
+console.log("Long food names:", longFoodNames);
+console.log("Short food names:", shortFoodNames);
 
+if (longFoodNames.length > shortFoodNames.length) {
+  console.log("There are more long-named foods.");
+} else if (longFoodNames.length < shortFoodNames.length) {
+  console.log("There are more short-named foods.");
+} else {
+  console.log("There are an equal number of long and short-named foods.");
+}
 
 // 10. STRETCH: Find the longest food name and print:
-// "The longest food name in the list is ______ with ___ characters."
+let longestFood = friendFavorites.reduce((longest, current) =>
+  current.length > longest.length ? current : longest
+);
+console.log(`The longest food name in the list is ${longestFood} with ${longestFood.length} characters.`);
